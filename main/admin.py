308b262f111b584f1132a950bd63155bc7dfdf46
zzzZZZZZZZZ FATSLUTBOOBIEMILK <polika1213sdf@gmail.com>
2025-06-28 16:50:52 +0300

    Update admin.py mes

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,12 @@
-# admin.py
 from django.contrib import admin
 from .models import ContactMessage
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
-    readonly_fields = ('name', 'email', 'message', 'created_at')
+    
+    # Для не-суперпользователей
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return ['name', 'email', 'message', 'created_at']
+        return []
